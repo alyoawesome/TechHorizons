4a17add4990834590850ee769bf6289fcaeef172
Alex <alyoawesome100@gmail.com>
2023-04-01 03:00:00 -0700

    added pages for the store and route config

---
+++ front-end/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+ 
+
+  <div class="page-wrap">
+
+
+  </div>
+
+</template>
+
+
+
+<script>
+
+
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+  data() {
+    return{
+      user: null,
+    }
+  },
+}
+  
+</script> 
---
+++ front-end/src/main.js
@@ -0,0 +1,33 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './main.css';
+import * as VueRouter from 'vue-router'
+import ShoppingCartPage from './pages/ShoppingCartPage.vue';
+import ProductsPage from './pages/ProductsPage.vue';
+import ProductDetailPage from './pages/ProductDetailPage.vue';
+import NotFoundPage from './pages/NotFoundPage.vue';
+
+
+createApp(App)
+.use(VueRouter.createRouter({
+  history: VueRouter.createWebHistory(process.env.BASE_URL),
+  routes: [{
+    path: '/cart',
+    component: ShoppingCartPage,
+  }, {
+    path: '/products',
+    component: ProductsPage,
+  }, {
+    path: '/products/:productId',
+    component: ProductDetailPage,
+  }, {
+    path: '/',
+    redirect: '/products',
+
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: NotFoundPage,
+  }]
+}))
+.mount('#app')
