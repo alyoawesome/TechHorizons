27dbefd1b733db9522e9b2595eceac0930c718ec
Alex <alyoawesome100@gmail.com>
2023-04-03 03:00:00 -0700

    Added navbar and product component for use in pages

---
+++ front-end/src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="nav-bar">
+    <router-link to="/products" class="products-link">
+      <div class="logo-wrap">
+        <img class="logo-size" :src=logo2 />
+      </div>
+    </router-link>
+
+
+  </div>
+</template>
+
+<script>
+
+import logo2 from '../assets/logo2.png';
+
+export default {
+  name: "NavBar",
+  props: ["user"],
+  data() {
+    return {
+      logo2,
+    }
+  },
+  
+}
+</script>
---
+++ front-end/src/components/ShoppingCartList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="product-container" v-for="product in products" :key="product.id">
+    <img class="product-image" :src="product.imageUrl" />
+    <div class="details-wrap">
+      <h3>{{ product.name }}</h3>
+      <p>{{ product.price }}</p>
+
+</template>
+
+<script>
+export default {
+  name: "ShoppingCartList",
+  props: ['products']
+}
+</script>
