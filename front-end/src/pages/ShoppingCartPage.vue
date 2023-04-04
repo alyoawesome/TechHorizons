397e3aac37724d7315c474c7f701844f5b62081a
Alex <alyoawesome100@gmail.com>
2023-04-04 08:00:00 -0700

    Implemented basic frontend for Shopping Cart page

---
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>Shopping Cart</h1>
+    <div v-if="cartItems.length > 0">
+      <ShoppingCartList :products="cartItems" />
+      <button class="checkout-button">Proceed to Checkout</button>
+    </div>
+    <div v-if="cartItems.length === 0">
+      You current have no items in your cart!
+    </div>
+  </template>
+  
+  <script>
+  import ShoppingCartList from '@/components/ShoppingCartList.vue';
+  import { cartItems } from '../temp-data';
+  
+  export default {
+    name: "ShoppingCartPage",
+    components: {
+      ShoppingCartList,
+    },
+    data() {
+      return {
+        cartItems,
+      }
+    }
+  }
+  </script>
