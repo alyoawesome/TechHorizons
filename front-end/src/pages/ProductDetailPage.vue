dc21de11580e7454f1e792885c2befde2efc049f
Alex <alyoawesome100@gmail.com>
2023-04-04 05:00:00 -0700

    Implemented basic frontend for Products and ProductDetailsPage

---
+++ front-end/src/pages/ProductDetailPage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div v-if="product">
+        <div class="img-wrap">
+          <img :src="product.imageName" />
+        </div>
+        <div class="product-details">
+          <h1>{{ product.name }}</h1>
+          <h3 class="price">{{ product.price }}</h3>
+          <button class="add-to-cart">Add to cart</button>
+        </div>
+    </div>
+    <div v-else>
+      <NotFoundPage />
+    </div>
+  </template>
+  
+  <script>
+  import { products } from '../temp-data';
+  import NotFoundPage from './NotFoundPage.vue';
+  
+  export default {
+    name: "ProductDetailPage",
+    data() {
+      return {
+        product: products.find(product => product.id === this.$route.params.productId),
+      }
+    },
+    components: {
+      NotFoundPage
+    },
+  }
+  </script>
---
+++ front-end/src/pages/ProductsPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Products</h1>
+    <ProductsList :products="products"/>
+  </template>
+  
+  <script>
+  import ProductsList from '../components/ProductsList.vue';
+  import { products } from '../temp-data';
+  
+  export default {
+    name: "ProductsPage",
+    components: {
+      ProductsList,
+    },
+    data() {
+      return {
+        products,
+      }
+    }
+  }
+  </script>
