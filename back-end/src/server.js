84869476570da6c65c84ea28603a7df0394d5ab3
Alex <alyoawesome100@gmail.com>
2023-04-11 06:00:00 -0700

    developed get api calls in backend

---
+++ back-end/src/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { cartItems as cartItemsRaw, products as productsRaw } from './temp-data';
+
+let cartItems = cartItemsRaw;
+let products = productsRaw;
+
+const app = express();
+app.use(express.json());
+
+app.get('/hello', (req, res) => {
+  res.send('Hello!');
+});
+
+app.get('/products', (req, res) => {
+  res.json(products);
+});
+
+function populateCartIds(ids) {
+  return ids.map(id => products.find(product => product.id === id));
+}
+
+app.get('/cart', (req, res) => {
+  const populatedCart = populateCartIds(cartItems);
+  res.json(populatedCart);
+});
+
+app.get('/products/:productId', (req, res) => {
+  const productId = req.params.productId;
+  const product = products.find(product => product.id === productId);
+  res.json(product);
+});
