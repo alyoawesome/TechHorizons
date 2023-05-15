7b0db870de7f3d37b6b4457eac58ea0dbe6480db
Alex <alyoawesome100@gmail.com>
2023-05-15 09:00:00 -0700

    corrected get calls and added post call for adding to cart in products detail page

--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -1,12 +1,13 @@
 <template>
     <div v-if="product">
         <div class="img-wrap">
-          <img :src="product.imageName" />
+          <img :src="product.imageUrl" />
         </div>
         <div class="product-details">
           <h1>{{ product.name }}</h1>
           <h3 class="price">{{ product.price }}</h3>
-          <button class="add-to-cart">Add to cart</button>
+          <button @click="addToCart" class="add-to-cart" v-if="!itemIsInCart">Add to cart</button>
+          <button class="grey-button" v-if="itemIsInCart">Item is already in cart</button>
         </div>
     </div>
     <div v-else>
@@ -15,18 +16,39 @@
   </template>
   
   <script>
-  import { products } from '../temp-data';
+  import axios from 'axios';
   import NotFoundPage from './NotFoundPage.vue';
   
   export default {
     name: "ProductDetailPage",
     data() {
       return {
-        product: products.find(product => product.id === this.$route.params.productId),
+        product: {},
+        cartItems: [],
+      }
+    },
+    computed: {
+      itemIsInCart() {
+        return this.cartItems.some(item => item.id === this.$route.params.productId);
+      }
+    },
+    methods: {
+      async addToCart() {
+        await axios.post('/api/users/12345/cart', { id: this.$route.params.productId });
+        alert('Successfully added item to cart!');
       }
     },
     components: {
       NotFoundPage
     },
+    async created() {
+      const response = await axios.get(`/api/products/${this.$route.params.productId}`);
+      const product = response.data;
+      this.product = product;
+  
+      const cartResponse = await axios.get('/api/users/12345/cart');
+      const cartItems = cartResponse.data;
+      this.cartItems = cartItems;
+    }
   }
   </script>
