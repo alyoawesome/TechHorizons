9e41bd7d240e7da327c89dbab2f9feffc4a407e0
Alex <alyoawesome100@gmail.com>
2023-05-16 03:00:00 -0700

    added delete and get calls in shooping cart page

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Fullstack E-Commerce Website
 
-Frontend: Used Vue.js
-Backend: Used Node.js Express.js, MongoDB, and Axios to communicate between the front and back ends
-Added authentication with Firebase Authentication and deployed this application on render.
+Frontend: Used Vue.js to develop the products, product details, and shopping cart pages<br><br>
+Backend: Used Node.js and Express.js to develop REST API calls for getting products from the database, along with adding and deleting items from a user's cart. Used Axios to communicate between the front and back ends <br><br>
+Database: Used MongoDB to store products, user and product ids, and users' carts. <br><br>
+
--- front-end/src/components/ShoppingCartList.vue
+++ front-end/src/components/ShoppingCartList.vue
@@ -1,15 +1,39 @@
 <template>
-  <div class="product-container" v-for="product in products" :key="product.id">
-    <img class="product-image" :src="product.imageUrl" />
-    <div class="details-wrap">
-      <h3>{{ product.name }}</h3>
-      <p>{{ product.price }}</p>
-
+  <h1>Shopping Cart</h1>
+  <div v-if="cartItems.length > 0">
+    <ShoppingCartList @remove-from-cart="removeFromCart($event)" :products="cartItems" />
+    <button class="checkout-button">Proceed to Checkout</button>
+  </div>
+  <div v-if="cartItems.length === 0">
+    You current have no items in your cart!
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+import ShoppingCartList from '@/components/ShoppingCartList.vue';
+
 export default {
-  name: "ShoppingCartList",
-  props: ['products']
+  name: "ShoppingCartPage",
+  components: {
+    ShoppingCartList,
+  },
+  data() {
+    return {
+      cartItems: [],
+    }
+  },
+  methods: {
+    async removeFromCart(productId) {
+      const response = await axios.delete(`/api/users/12345/cart/${productId}`);
+      const updatedCart = response.data;
+      this.cartItems = updatedCart;
+    },
+  },
+  async created() {
+    const response = await axios.get('/api/users/12345/cart');
+    const cartItems = response.data;
+    this.cartItems = cartItems;
+  }
 }
 </script>
