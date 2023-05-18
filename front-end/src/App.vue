3aa16ecff5d005ecbb85bc9e405d6a96ea130293
Alex <alyoawesome100@gmail.com>
2023-05-18 03:00:00 -0700

    adding firebase auth to app.vue file

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,16 +1,16 @@
 <template>
- 
 
+  <NavBar :user="user"/>
   <div class="page-wrap">
 
-
+      <router-view :user="user"></router-view>
   </div>
 
 </template>
 
-
-
 <script>
+import {getAuth, onAuthStateChanged} from 'firebase/auth';
+import NavBar from '@/components/NavBar.vue'
 
 
 export default {
@@ -23,6 +23,13 @@ export default {
       user: null,
     }
   },
-}
-  
+  // centralized way to check if logged in our not (Because can pass the user object down to any of the pages with props)
+  created() {
+    const auth = getAuth();
+   
+    onAuthStateChanged(auth, user => {
+      this.user = user
+    });
+  }
+};
 </script> 
