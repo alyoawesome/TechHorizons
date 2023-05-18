d55b238f35d81b3430d0856013c4acbcfa62e5d7
Alex <alyoawesome100@gmail.com>
2023-05-18 04:00:00 -0700

    corrected firebase auth info and api key in main js file

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -8,6 +8,28 @@ import ProductDetailPage from './pages/ProductDetailPage.vue';
 import NotFoundPage from './pages/NotFoundPage.vue';
 
 
+import { initializeApp } from "firebase/app";
+
+//Configuration to connect to Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCfrP7uZhUlcSV1P7xD2Y6sDyzdQ8kSykw",
+  authDomain: "techhorizons-16e99.firebaseapp.com",
+  projectId: "techhorizons-16e99",
+  storageBucket: "techhorizons-16e99.appspot.com",
+  messagingSenderId: "528155349507",
+  appId: "1:528155349507:web:bed4fe3b8f5b3788ca3436"
+};
+
+
+
+
+
+
+initializeApp(firebaseConfig);
+
+
+
+
 createApp(App)
 .use(VueRouter.createRouter({
   history: VueRouter.createWebHistory(process.env.BASE_URL),
