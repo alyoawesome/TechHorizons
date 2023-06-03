77a079150053d3171a873c751121db51b0c44aff
Alex <alyoawesome100@gmail.com>
2023-06-03 02:00:00 -0700

    readied backend for deployment on render

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,65 +1,101 @@
 import express from 'express';
-import { MongoClient } from 'mongodb';
+import {MongoClient} from 'mongodb';
+import path from 'path';
+
 
 async function start() {
- const url = 'mongodb+srv://fs-server:Sfpsf7KT725Lh3v9@cluster0.4bhpjnw.mongodb.net/?retryWrites=true&w=majority&ssl=true'
+
+  const url = 'mongodb+srv://fs-server:Sfpsf7KT725Lh3v9@cluster0.4bhpjnw.mongodb.net/?retryWrites=true&w=majority&ssl=true'
   const client = new MongoClient(url);
 
   await client.connect();
   const db = client.db('fsv-db');
-
   const app = express();
-  app.use(express.json());
 
-  app.get('/products', async (req, res) => {
+
+  app.use(express.json());
+  app.use('/images', express.static(path.join(__dirname, '../assets')));
+
+  app.use(express.static(
+    path.resolve(__dirname, "../dist"),
+    { maxAge: "1y", etag: false },
+    ));
+
+  app.get('/api/products', async (req, res) => {
     const products = await db.collection('products').find({}).toArray();
-    res.send(products);
+    res.send(products); 
   });
 
+
   async function populateCartIds(ids) {
+
     return Promise.all(ids.map(id => db.collection('products').findOne({ id })));
   }
 
-  app.get('/users/:userId/cart', async (req, res) => {
-    const user = await db.collection('users').findOne({ id: req.params.userId });
-    const populatedCart = await populateCartIds(user.cartItems);
+
+  app.get('/api/users/:userId/cart', async (req, res) => {
+    const user = await db.collection('users').findOne({id: req.params.userId});
+
+    const populatedCart = await populateCartIds(user?.cartItems || []);
     res.json(populatedCart);
   });
 
-  app.get('/products/:productId', async (req, res) => {
+  app.get('/api/products/:productId', async (req, res) => {
     const productId = req.params.productId;
-    const product = await db.collection('products').findOne({ id: productId });
-    res.json(product);
+    const product = await db.collection('products').findOne({id: productId});
+    res.json(product); 
   });
 
-  app.post('/users/:userId/cart', async (req, res) => {
+
+
+
+
+  app.post('/api/users/:userId/cart', async(req, res) => {
     const userId = req.params.userId;
+
     const productId = req.body.id;
 
-    await db.collection('users').updateOne({ id: userId }, {
-      $addToSet: { cartItems: productId }
-    });
+    const existingUser = await db.collection('users').findOne({id: userId});
 
-    const user = await db.collection('users').findOne({ id: req.params.userId });
-    const populatedCart = await populateCartIds(user.cartItems);
+
+    if (!existingUser){
+       await db.collection('users').insertOne({id: userId, cartItems:[]});
+    }
+
+
+    await db.collection('users').updateOne({id: userId}, {$addToSet: {cartItems: productId}});
+
+
+    const user = await db.collection('users').findOne({id: req.params.userId});
+    const populatedCart = await populateCartIds(user?.cartItems || []);
     res.json(populatedCart);
   });
 
-  app.delete('/users/:userId/cart/:productId', async (req, res) => {
+
+
+  app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
     const userId = req.params.userId;
     const productId = req.params.productId;
 
-    await db.collection('users').updateOne({ id: userId }, {
-      $pull: { cartItems: productId },
-    });
+    await db.collection('users').updateOne({id: userId}, {$pull: {cartItems: productId}});
 
-    const user = await db.collection('users').findOne({ id: req.params.userId });
-    const populatedCart = await populateCartIds(user.cartItems);
+    const user = await db.collection('users').findOne({id: req.params.userId});
+    const populatedCart = await populateCartIds(user?.cartItems || []);
     res.json(populatedCart);
+
+  
+
   });
 
-  app.listen(8000, () => {
-    console.log('Server is listening on port 8000')
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../dist/index.html"));
+  });
+
+  const port = process.env.PORT || 8081;
+
+  app.listen(port, () => {
+    console.log('Server is listening on port' + " " + port);
   });
 }
 
